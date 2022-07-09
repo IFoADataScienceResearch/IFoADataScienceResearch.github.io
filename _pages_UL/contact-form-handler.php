91c4ca0fe7a0f349c82b4dde47eb39aad776b973
Debashish Dey <50278638+debsdey@users.noreply.github.com>
2022-07-09 14:24:13 +0100

    Update contact-form-handler.php

--- _pages_UL/contact-form-handler.php
+++ _pages_UL/contact-form-handler.php
@@ -1,6 +1,6 @@
 <?php 
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
+$myemail = 'debsdey@hotmail.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
